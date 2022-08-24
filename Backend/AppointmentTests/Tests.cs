2b753fa04ad1c9a765155195363e3968d49004a2
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2022-08-24 16:13:18 -0600

    Update Tests.cs

--- Backend/AppointmentTests/Tests.cs
+++ Backend/AppointmentTests/Tests.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore.InMemory;
 using Microsoft.EntityFrameworkCore;
 using AppointmentServiceApi;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AppointmentTests
 {
@@ -15,26 +17,29 @@ namespace AppointmentTests
             options = new DbContextOptionsBuilder<AppointmentDb>()
             .UseInMemoryDatabase(databaseName: "EmployeeDataBase")
             .Options;
-            
-            
+
+            context = new AppointmentDb(options);
+
         }
 
 
         [Fact]
         public async void TestGetAll()
         {
-            using(context = new AppointmentDb(options))
-            {
-                context.Appointments.Add(new Appointment { VendorId = 1, CustomerId = 2, JobId = 1 });
-                context.Appointments.Add(new Appointment { VendorId = 1, CustomerId = 3, JobId = 2 });
-                context.Appointments.Add(new Appointment { VendorId = 1, CustomerId = 4, JobId = 3 });
-                context.SaveChanges();
-            }
-
+            context.Appointments.Add(new Appointment { VendorId = 1, CustomerId = 2, JobId = 1 });
+            context.Appointments.Add(new Appointment { VendorId = 1, CustomerId = 3, JobId = 2 });
+            context.Appointments.Add(new Appointment { VendorId = 1, CustomerId = 4, JobId = 3 });
+            context.SaveChanges();
             AppointmentsController controller = new AppointmentsController(context);
 
-            var appointments = await controller.GetAllAppointments();
-            Assert.NotNull(appointments);
+            var result = await controller.GetAllAppointments();
+
+            var actual = (result.Result as OkObjectResult).Value as IEnumerable<Appointment>;
+            Assert.NotNull(actual);
+            Assert.Equal(3 , actual.Count());
+            Assert.Equal(1 , actual.First().VendorId);
+            Assert.Equal(2 , actual.First().CustomerId);    
+            Assert.Equal(1 , actual.First().JobId);
 
         }
     }
