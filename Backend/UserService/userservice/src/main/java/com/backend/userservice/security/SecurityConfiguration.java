80de429e0662398da4b02724cd40f7470cb7ea45
Poke711 <wmejiapalma@student.neumont.edu>
2022-08-25 15:31:20 -0600

    (README ADDED) Added Very Basic admin auth on any DELETE request

---
+++ Backend/UserService/userservice/src/main/java/com/backend/userservice/security/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.backend.userservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MyUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers(HttpMethod.DELETE, "/*").hasRole("ADMIN")
+        .antMatchers("/*").permitAll()
+        .anyRequest().authenticated()
+        .and().httpBasic()
+        .and().csrf().disable();
+    }
+    
+    //Create Password Encoder with plain text
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+    
+}
--- Backend/UserService/userservice/src/main/java/com/backend/userservice/services/UserService.java
+++ Backend/UserService/userservice/src/main/java/com/backend/userservice/services/UserService.java
@@ -29,12 +29,28 @@ public class UserService {
         }
         return repo.findById(id).get();
     }
-    public void editUser(User user){
-        repo.deleteById(user.getId());
-        repo.insert(user);
+    public boolean editUser(String id, User user){
+        repo.findById(id).ifPresent(u -> {
+            u.setName(user.getName());
+            u.setPhonenumber(user.getPhonenumber());
+            u.setEmail(user.getEmail());
+            u.setAddress(user.getAddress());
+            u.setAppointments(user.getAppointments());
+            u.setBio(user.getBio());
+            u.setPassword(user.getPassword());
+            u.setVender(user.isVender());
+            repo.save(u);
+        } );
+        return repo.findById(id).isPresent();
     }
-    public void deleteUser(User user){
-        repo.deleteById(user.getId());
+    public User deleteUser(User user){
+        try{
+            repo.deleteById(user.getId());
+            return user;
+        }
+        catch(Exception e){
+            return null;
+        }
     }
     public List<String> getUserAppointments(User user){
         //throw not yet implemented
