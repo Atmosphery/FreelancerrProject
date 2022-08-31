5d68d8fe88ae0d5d03323c1fbb58b67392e12945
nathanielmalmrose <79717751+nathanielmalmrose@users.noreply.github.com>
2022-08-30 23:12:35 -0600

    some appointment logic front end

--- Frontend/freelancerr_frontend/lib/AppCard.dart
+++ Frontend/freelancerr_frontend/lib/AppCard.dart
@@ -1,14 +1,23 @@
-// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, prefer_interpolation_to_compose_strings
 
 import 'package:flutter/material.dart';
+import 'package:freelancerr_frontend/AppPage.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import 'AppModel.dart';
 import 'User.dart';
 
 class AppCard extends StatefulWidget {
-  AppCard({Key? key, required this.user, required this.appModel})
+  AppCard(
+      {Key? key,
+      required this.user,
+      required this.appModel,
+      required this.userDisplay})
       : super(key: key);
   final User user;
+  final String userDisplay;
   final AppModel appModel;
 
   @override
@@ -16,10 +25,28 @@ class AppCard extends StatefulWidget {
 }
 
 class _AppCardState extends State<AppCard> {
+  String? userName = "";
+
+  @override
+  void initState() {
+    getUserName();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        print("Id: " + widget.appModel.id.toString());
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => AppPage(
+                    app: widget.appModel,
+                    user: widget.user,
+                  )),
+        );
+      },
       child: Container(
         width: MediaQuery.of(context).size.width,
         child: Column(
@@ -29,7 +56,7 @@ class _AppCardState extends State<AppCard> {
                 radius: 30,
               ),
               title: Text(
-                widget.user.name!,
+                userName!,
                 style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
@@ -60,5 +87,16 @@ class _AppCardState extends State<AppCard> {
         ),
       ),
     );
+  }
+
+  Future<void> getUserName() async {
+    var url = Uri.parse(
+        'http://localhost:8888/userservice/user/${widget.userDisplay}');
+    http.Response response = await http.get(url);
+
+    print('name string: ' + User.fromJson(jsonDecode(response.body)).name!);
+    setState(() {
+      userName = User.fromJson(jsonDecode(response.body)).name!;
+    });
   }
 }
--- Frontend/freelancerr_frontend/lib/CustomList.dart
+++ Frontend/freelancerr_frontend/lib/CustomList.dart
@@ -1,72 +1,72 @@
-// ignore_for_file: must_be_immutable
+// // ignore_for_file: must_be_immutable
 
-import 'package:flutter/material.dart';
-import 'package:freelancerr_frontend/InfoTile.dart';
-import 'package:freelancerr_frontend/Job.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+// import 'package:flutter/material.dart';
+// import 'package:freelancerr_frontend/InfoTile.dart';
+// import 'package:freelancerr_frontend/Job.dart';
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
 
-class CustomList extends StatefulWidget {
-  CustomList({
-    Key? key,
-  }) : super(key: key);
+// class CustomList extends StatefulWidget {
+//   CustomList({
+//     Key? key,
+//   }) : super(key: key);
 
-  @override
-  State<CustomList> createState() => _CustomListState();
-}
+//   @override
+//   State<CustomList> createState() => _CustomListState();
+// }
 
-class _CustomListState extends State<CustomList> {
-  List<InfoTile> items = [];
-  List<Job> jobs = [];
+// class _CustomListState extends State<CustomList> {
+//   List<InfoTile> items = [];
+//   List<Job> jobs = [];
 
-  @override
-  void initState() {
-    refreshJobs();
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     refreshJobs();
+//     super.initState();
+//   }
 
-  Future refreshJobs() async {
-    Uri uri = Uri.parse("http://localhost:8888/jobs/all");
-    final response = await http.get(uri);
-    var data = json.decode(response.body);
-    //jobs = data;
-    this.items = [];
-    setState(() {
-      for (var i = 0; i < data.length; i++) {
-        this.items.add(
-              InfoTile(
-                title: data[i]['title'],
-                image: Image.network(
-                  data[i]['imageLink'],
-                ),
-                jobId: data[i]['id'],
-                userId: data[i]['userId'],
-              ),
-            );
-      }
-    });
-  }
+//   Future refreshJobs() async {
+//     Uri uri = Uri.parse("http://localhost:8888/jobs/all");
+//     final response = await http.get(uri);
+//     var data = json.decode(response.body);
+//     //jobs = data;
+//     this.items = [];
+//     setState(() {
+//       for (var i = 0; i < data.length; i++) {
+//         this.items.add(
+//               InfoTile(
+//                 title: data[i]['title'],
+//                 image: Image.network(
+//                   data[i]['imageLink'],
+//                 ),
+//                 jobId: data[i]['id'],
+//                 userId: data[i]['userId'],
+//               ),
+//             );
+//       }
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    ScrollController controller = ScrollController();
+//   @override
+//   Widget build(BuildContext context) {
+//     ScrollController controller = ScrollController();
 
-    return RefreshIndicator(
-      onRefresh: refreshJobs,
-      child: ListView.builder(
-        physics: const AlwaysScrollableScrollPhysics(),
-        controller: controller,
-        shrinkWrap: true,
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return InfoTile(
-            title: items[index].title,
-            image: items[index].image,
-            jobId: items[index].jobId,
-            userId: items[index].userId,
-          );
-        },
-      ),
-    );
-  }
-}
+//     return RefreshIndicator(
+//       onRefresh: refreshJobs,
+//       child: ListView.builder(
+//         physics: const AlwaysScrollableScrollPhysics(),
+//         controller: controller,
+//         shrinkWrap: true,
+//         itemCount: items.length,
+//         itemBuilder: (context, index) {
+//           return InfoTile(
+//             title: items[index].title,
+//             image: items[index].image,
+//             jobId: items[index].jobId,
+//             userId: items[index].userId,
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
