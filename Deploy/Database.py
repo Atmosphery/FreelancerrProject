4689188faa352bcf68aa75a89841a80577bb4d5f
Poke711 <wmejiapalma@student.neumont.edu>
2022-08-30 19:09:12 -0600

    Added Job Service and fixed bug with script
    script was pushing users to the wrong collection

--- Deploy/Database.py
+++ Deploy/Database.py
@@ -12,7 +12,10 @@ UseDEBUG = False
 
 sql_string = ""
 mongo_string = ""
+jobs_string=""
 sql_connection, sql_cursor, mongo_connection, mongo_collection = None,None,None,None
+jobs_connection, jobs_cursor = None, None
+connection_attempts = 0
 
 
 
@@ -33,16 +36,28 @@ def insert_data_sql():
         INSERT INTO dbo.Appointments (id, status, vendorid, customerid, jobid, appointmentdate)
         VALUES (?, ?, ?, ?, ?, ?)
         """, item["id"], item["status"], item["vendorid"], item["customerid"], item["jobid"], item["appointmentdate"])
-
     sql_connection.commit()
+def insert_data_jobs():
+    f = open("sql_data.json", "r")
+    data = json.load(f)
+    f.close()
+    for item in data["jobs_data"]:
+        print(item)
+        jobs_cursor.execute("SET IDENTITY_INSERT Jobs ON")
+        jobs_cursor.execute("""
+        INSERT INTO dbo.Jobs (id, title, description, userid, baseprice, imagelink)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """, item["id"], item["title"], item["description"], item["userid"], item["baseprice"], item["imagelink"])
+    jobs_connection.commit()
 def debugthings():
+    global sql_string, mongo_string, jobs_string
     if(UseDEBUG):
         sql_string = "DRIVER={ODBC Driver 17 for SQL Server}; SERVER=LAPTOP-6H5NQFS0; Database=testdata; Trusted_Connection=yes;"
         mongo_string = "mongodb://localhost:27017/"
     else:
         sql_string = "DRIVER={ODBC Driver 17 for SQL Server}; SERVER=tcp:127.0.0.1,1500; Database=AppointmentDb; UID=SA; PWD=abc123!!@;"
         mongo_string = "mongodb://root:root@localhost:27018/?authMechanism=DEFAULT&authSource=admin"
-    return sql_string, mongo_string
+        jobs_string = "DRIVER={ODBC Driver 17 for SQL Server}; SERVER=tcp:127.0.0.1,1501; Database=JobDb; UID=sa; PWD=abc123!!@; "
 
 def insert_data():
     print("Inserting Data into SQL")
@@ -68,37 +83,47 @@ def create_table_if_not_exist(tablename):
             );
                         """)
 def setup_database_connections():
-    sql_string, mongo_string = debugthings()
-    
-    print("Connecting to SQL Database...")
+    global sql_connection, sql_cursor, mongo_connection, mongo_collection, jobs_connection, jobs_cursor
+    debugthings()
+    print("Attempting to Connect to SQL Databases...")
+    jobs_connection = pyodbc.connect(jobs_string)
+    jobs_cursor = jobs_connection.cursor()
+    print("Connected to Jobs Database")
     sql_connection = pyodbc.connect(sql_string)
     sql_cursor = sql_connection.cursor()
+    print("Connected to Appointment Database")
 
     print("Connecting to Mongo Database....")
     mongo_connection = MongoClient(mongo_string)
-    mongo_db = mongo_connection.testdata
-    mongo_collection = mongo_db.persons
+    mongo_db = mongo_connection.users
+    mongo_collection = mongo_db.user
     print("Successfully Connected to SQL and Mongo Databases")
-    return sql_connection, sql_cursor, mongo_connection, mongo_collection
 
 def close_database_connections():
     sql_connection.close()
     mongo_connection.close()
+    jobs_connection.close()
 
 def setup_databases_with_data():
-    global sql_connection, sql_cursor, mongo_connection, mongo_collection
-    sql_connection, sql_cursor, mongo_connection, mongo_collection = setup_database_connections()
+    setup_database_connections()
     insert_data_sql()
     insert_data_mongo()
+    insert_data_jobs()
     print("Successfully setup databases with data")
     print("Please wait 30 seconds for eureka and api gateway to register services")
     close_database_connections()
 def wait_for_database():
+    global connection_attempts
     while True:
         try:
-            sql_connection, sql_cursor, mongo_connection, mongo_collection = setup_database_connections()
+            setup_database_connections()
             break
         except Exception as e:
+            connection_attempts += 1
+            if connection_attempts > 10:
+                raise Exception("Could not connect to databases")
+                break
             time.sleep(5)
 if __name__ == "__main__":
+    setup_databases_with_data()
     print("This is a module please run Main.py")
