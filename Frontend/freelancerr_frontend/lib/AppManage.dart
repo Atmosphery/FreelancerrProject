470364d9494229b3e2e6f956df920ae7f937d5f6
nathanielmalmrose <79717751+nathanielmalmrose@users.noreply.github.com>
2022-08-30 18:23:42 -0600

    Revert "update"
    This reverts commit f710b4f63d44faebcdaffe68dc5ab4471574a5da.

--- Frontend/freelancerr_frontend/lib/AppManage.dart
+++ Frontend/freelancerr_frontend/lib/AppManage.dart
@@ -1,17 +1,12 @@
-// ignore_for_file: prefer_const_constructors_in_immutables, must_be_immutable, constant_identifier_names, prefer_const_literals_to_create_immutables, prefer_const_constructors, sized_box_for_whitespace, unnecessary_new, prefer_interpolation_to_compose_strings
-
-import 'dart:convert';
+// ignore_for_file: prefer_const_constructors_in_immutables, must_be_immutable, constant_identifier_names, prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
-import 'AppCard.dart';
 import 'AppCreate.dart';
-import 'AppModel.dart';
 import 'CustomAppBar.dart';
 import 'CustomDrawerWidget.dart';
 import 'CustomList.dart';
 import 'User.dart';
-import 'package:http/http.dart' as http;
 
 const String _AppTitle = "Freelancerr!";
 
@@ -20,8 +15,6 @@ class AppManage extends StatefulWidget {
       : super(key: key);
   final User user;
   List<User> venders;
-  //List<AppModel> apps;
-
   @override
   State<AppManage> createState() => _AppManageState();
 }
@@ -33,73 +26,26 @@ class _AppManageState extends State<AppManage> {
       appBar: CustomAppBar(title: _AppTitle),
       drawer: CustomDrawerWidget(
           header: _AppTitle, user: widget.user, venders: widget.venders),
-      body: Stack(
-        children: [
-          Center(
-            child: TextButton(
-              onPressed: () async {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return AppCreate(
-                        user: widget.user,
-                        venders: widget.venders,
-                      );
-                    },
-                  ),
-                );
-              },
-              child: Text("Add Appointment"),
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: FutureBuilder<List<AppModel>>(
-              future: getAppsUser(widget.user.id!),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                var data = snapshot.data!;
-                print(data);
-
-                return ListView.builder(
-                  itemCount: widget.venders.length,
-                  itemBuilder: (context, index) {
-                    print('${snapshot.data![index]}');
-                    return AppCard(
-                      appModel: snapshot.data![index],
+      body: Column(children: [
+        Center(
+          child: TextButton(
+            onPressed: () async {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return AppCreate(
                       user: widget.user,
+                      venders: widget.venders,
                     );
                   },
-                );
-              },
-            ),
+                ),
+              );
+            },
+            child: Text("Add Appointment"),
           ),
-        ],
-      ),
+        ),
+      ]),
     );
   }
 }
-
-Future<List<AppModel>> getAppsUser(String id) async {
-  var url = Uri.parse('http://localhost:8888/appointment/all/$id/vendor');
-  http.Response response = await http.get(url);
-
-  var appJson = jsonDecode(response.body);
-  List<AppModel> appUsersList = [];
-  for (int i = 0; i < appJson.length; i++) {
-    AppModel appToAdd = AppModel.fromJson(appJson[i]);
-    appUsersList.add(appToAdd);
-  }
-
-  for (int i = 0; i < appUsersList.length; i++) {
-    print("App User List: " + appUsersList[i].appointmentDate.toString());
-  }
-
-  return appUsersList;
-}
-
-
